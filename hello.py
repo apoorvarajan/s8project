abcd6fed9692abb4c7df4d48faa0afbc9b0c785a
ApoorvaRajan <apoorvarajan1997@gmail.com>
2019-05-04 20:18:49 +0530

    1st commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = os.path.join('static', 'img')
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/')
+def upload_file():
+   return render_template('page1.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename("picture.png")))
+   return redirect(url_for('upload_file'))
+def color():
+   model_predict.model_predict()
+	
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ static/js/page1.js
@@ -0,0 +1,5 @@
+
+function picture(){
+    document.getElementById('sketch').style.display='block';
+    document.getElementById('colour').style.display='block';
+}
