de6c0af421b861de4ad4e2dc4d710c2797424141
ApoorvaRajan <apoorvarajan1997@gmail.com>
2019-05-04 23:35:55 +0530

    included model_predict

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 import os
-
+import cv2
+import model_predict
 UPLOAD_FOLDER = os.path.join('static', 'img')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -15,8 +16,12 @@ def uploader_file():
       f = request.files['file']
       f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename("picture.png")))
    return redirect(url_for('upload_file'))
+@app.route('/col' , methods=['GET', 'POST'])
 def color():
-   model_predict.model_predict()
+   img=cv2.imread(os.path.join('static','img',secure_filename("picture.png")))
+   model_predict.model_predict(img)
+   return redirect(url_for('upload_file'))
+
 	
 if __name__ == '__main__':
    app.run(debug = True)
